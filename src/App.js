cab896e0a50533b64fed4a2d851fd590fc6fe15c
Taiwo <taiwotunde32@gmail.com>
2022-02-09 00:50:33 +0100

    react redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddTodoForm from "./components/AddTodoForm";
+import "react-bootstrap";
+import TodoList from "./components/TodoList";
+import TotalCompleteItems from "./components/TotalCompleteItems";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todo List</h1>
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
     </div>
   );
 }
